7780223023845cc95fe4b560fba5be62713fe2cd
bcoe <bencoe@gmail.com>
2010-05-17 23:54:01 -0400

    Cleaned up some random junk laying around in folders -- left in some concrete examples of client-side validation and model loading, task scheduling, models and controllers.

--- app/tasks/grabStuff.js
+++
@@ -1,8 +0,0 @@
-var sys = require('sys');
-exports.GrabStuff = MobiusTask.extend({
-	frequency: 15000, // milliseconds
-	execute: function() {
-		// Your task logic here.
-		sys.puts('running task.');
-	}
-});
